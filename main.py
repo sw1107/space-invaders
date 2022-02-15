f2aca6cf7dfa81bfea0b18d96f3e6b64294d5bc8
sophiewhittle <sophiewhittle89@gmail.com>
2022-02-15 16:02:46 -0500

    update so that players don't reset every level

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from scoreboard import Scoreboard
 from enemy_controller import EnemyController
 from player_controller import PlayerController
 
-# TODO: change so that players don't reset every level?
 # TODO: change player and enemy shapes to space invader shapes
 # TODO: add sounds
 # TODO: build barriers
@@ -21,33 +20,29 @@ screen.tracer(0)
 screen.listen()
 
 scoreboard = Scoreboard()
+
 # ------------------- create controllers -------------------
 enemy_controller = EnemyController()
 player_controller = PlayerController()
+
+# ------------------- create players and add functionality -------------------
+player_controller.create_players()
+screen.onkey(player_controller.move_active_player_left, "Left")
+screen.onkey(player_controller.move_active_player_right, "Right")
+screen.onkey(player_controller.shoot, "space")
+
 
 # ------------------- set up game board -------------------
 
 
 def reset_game():
-
     for pellet in player_controller.player_pellets:
         player_controller.remove_player_pellet(pellet)
 
     for pellet in enemy_controller.enemy_pellets:
         enemy_controller.remove_enemy_pellet(pellet)
 
-    for player in player_controller.players:
-        player.hideturtle()
-
-    player_controller.players.clear()
-
-    player_controller.create_players()
-
     enemy_controller.create_enemies(scoreboard.level)
-
-    screen.onkey(player_controller.move_active_player_left, "Left")
-    screen.onkey(player_controller.move_active_player_right, "Right")
-    screen.onkey(player_controller.shoot, "space")
 
     screen.update()
 
